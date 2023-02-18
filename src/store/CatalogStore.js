01fc38577f43081c689530011df4f23cd02f0de7
evgeniyaili <ilinaevgeniya@mail.ru>
2023-02-18 18:18:35 +0300

    4

--- src/store/CatalogStore.js
+++ src/store/CatalogStore.js
@@ -1,16 +1,29 @@
 import { makeAutoObservable } from 'mobx'
 
 class CatalogStore {
-    _categories = []
-    _brands = []
-    _products = []
-    _category = null // выбранная категория
-    _brand = null // выбранный бренд
-    _page = 1 // текущая страница
-    _count = 0 // сколько всего товаров
-    _limit = 4 // товаров на страницу
-
+   
     constructor() {
+        _categories = [
+            {id: 1, name: 'Sneakers'},
+            {id: 2, name: 'T-Shirts'},
+            {id: 3, name: 'Hoodies'}
+        ]
+            _brands = []
+            _products = [
+                {id: 1, name: "Nike AF1", price: "2000000", img: `url(${star})`, info: 'kfjlskdjlkdjsdlkfj'},
+            {id: 6, name: "Nike RES", price: "2000000", img: ''},
+            {id: 7, name: "Nike ponse", price: "2000000", img: ''},
+            {id: 2, name: "Yeezy Boosts", price: "1231412", img: ''},
+            {id: 3, name: "Adidas Originals Hoodie", price: "202020", img: ''},
+            {id: 4, name: "Nike AirMax", price: "202020", img: ''},
+            {id: 5, name: "Nike Running", price: "202020", img: ''},
+            ]
+            _category = null // выбранная категория
+            _brand = null // выбранный бренд
+            _page = 1 // текущая страница
+            _count = 0 // сколько всего товаров
+            _limit = 4 // товаров на страницу
+        
         makeAutoObservable(this)
     }
 
